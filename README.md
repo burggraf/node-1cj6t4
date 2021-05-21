ea7ca01747117f53b279a72883d556f868f37db8
burggraf <markb@mantisbible.com>
2021-05-21 06:59:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-h3t5qb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-h3t5qb)
---
+++ index.js
@@ -0,0 +1,5 @@
+// Run `node index.js` in the terminal
+
+const m = require('lodash');
+console.log(`Hello Node.js v${process.versions.node}!`);
+console.log(typeof m);
